8a2d8a3372e825e7a9a4638bda5998ad0c4557d6
Talish <talishnazir@eguanacommerce.com>
2024-11-15 11:48:02 +0500

    Remove-Unused-Code

--- src/main/java/com/fullStack/ecommerce/graphql/ProductGraphQLResolver.java
+++ src/main/java/com/fullStack/ecommerce/graphql/ProductGraphQLResolver.java
@@ -9,7 +9,6 @@ import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 public class ProductGraphQLResolver {
--- src/main/java/com/fullStack/ecommerce/service/ProductService.java
+++ src/main/java/com/fullStack/ecommerce/service/ProductService.java
@@ -2,12 +2,10 @@ package com.fullStack.ecommerce.service;
 
 import com.fullStack.ecommerce.model.Product;
 import com.fullStack.ecommerce.repository.ProductRepository;
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -39,19 +37,6 @@ public class ProductService {
 
     public void deleteProduct(Long id) {
         productRepository.deleteById(id);
-    }
-
-    @PostConstruct
-    private void loadData() {
-        Product[] products = new Product[] {
-                Product.builder().name("Product 1").description("Product 1 Description").price(700.0).build(),
-                Product.builder().name("Product 2").description("Product 2 Description").price(1200.0).build(),
-                Product.builder().name("Product 3").description("Product 3 Description").price(450.0).build(),
-                Product.builder().name("Product 4").description("Product 4 Description").price(980.0).build()
-        };
-        for (Product product : products) {
-            createProduct(product);
-        }
     }
 }
 
